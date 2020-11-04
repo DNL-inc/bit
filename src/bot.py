e32efb99ba8a06bd75c0d2416d19bf6da0eef472
Ivan Romancev <dnl.krsvch@gmail.com>
2020-11-04 23:09:10 +0200

    fix wrong merge

--- src/bot.py
+++ src/bot.py
@@ -1,20 +1,17 @@
 from telebot import types
 import telebot
+from models.base import register_user, is_admin_bool
 
 bot = telebot.TeleBot(token='992816254:AAHc_pVKMqESQ84bjp_I80-AYertBBt7F80')
 
 
 def menu(message):
     chat_id = message.from_user.id
-    markup = types.ReplyKeyboardMarkup(row_width=2)
     schedule_item = types.KeyboardButton('Рaспиcание')
-    group_item = types.KeyboardButton('Група')
     if is_admin(message):
+    if is_admin_bool(message):
         admin_item = types.KeyboardButton('Админиcтрировaниe')
-        markup.add(schedule_item, admin_item, group_item)
     else:
-    bot.send_message(chat_id, "Выберіть щось", reply_markup=markup)
-        markup.add(schedule_item, group_item)
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -27,51 +24,21 @@ def start(message):
 def response_menu(message):
     if message.text == "Рaспиcание": response_schedule(message)
     elif message.text == "Админиcтрировaниe": pass
-    elif message.text == "Група": choose_course(message)
 
-def choose_course(message):
-    chat_id = message.from_user.id
     markup = types.InlineKeyboardMarkup(row_width=1)
-    firstCourse = types.InlineKeyboardButton(text='1-й курс', callback_data="first") 
-    secondCourse = types.InlineKeyboardButton(text='2-й курс', callback_data="second") 
-    thirrdCourse = types.InlineKeyboardButton(text='3-й курс', callback_data="third") 
-    fourthCouse = types.InlineKeyboardButton(text='4-й курс', callback_data="fourth") 
-    fifthCourse = types.InlineKeyboardButton(text='5-й курс', callback_data="fifth") 
-    sixthCourse = types.InlineKeyboardButton(text='6-й курс', callback_data="sixth")
-    markup.add(firstCourse, secondCourse, thirrdCourse, fourthCouse, fifthCourse, sixthCourse)
-    bot.send_message(chat_id, 'Виберіть курс:', reply_markup=markup)
-    
-
-def choose_group(message):
-    chat_id = message.from_user.id
-    markup = types.InlineKeyboardMarkup(row_width=2)
-    group1 = types.InlineKeyboardButton(text='ІПЗ-11', callback_data="ipz11")
-    group2 = types.InlineKeyboardButton(text='ІПЗ-12', callback_data="ipz12")
-    group3 = types.InlineKeyboardButton(text='ІПЗ-13', callback_data="ipz13")
-    markup.add(group1, group2, group3)
-    bot.send_message(chat_id, 'Виберіть группу:', reply_markup=markup)
-
-    
 def response_schedule(message):
     bot.delete_message(message_id=message.message_id, chat_id=message.from_user.id)
     chat_id = message.from_user.id
     markup = types.InlineKeyboardMarkup(row_width=1)
     monday = types.InlineKeyboardButton(
-        text='Понеділок', callback_data="schedule_monday")
     tuesday = types.InlineKeyboardButton(
-        text='Вівторок', callback_data="schedule_tuesday")
     wednesday = types.InlineKeyboardButton(
-        text='Середа', callback_data="schedule_wednesday")
     thursday = types.InlineKeyboardButton(
-        text='Четвер', callback_data="schedule_thursday")
     friday = types.InlineKeyboardButton(
-        text='П\'ятниця', callback_data="schedule_friday")
     markup.add(monday, tuesday, wednesday, thursday, friday)
-    bot.send_message(chat_id, 'Виберіть день:', reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: True)
-def handler_calls(call):
     if call.data == 'schedule_monday':
         bot.answer_callback_query(call.id, text="""
         Ох, понедельние будет тяжёлым...
@@ -120,7 +87,6 @@ def handler_calls(call):
 12:10 - 13:30 [Іноземна мова(П)](https://us04web.zoom.us/j/7591896666?pwd=OHJhNjRRSTJxMUN3V1ZBTGdhMFVxZz09&nbsp;)
 13:40 - 15:00 Chill time
 """, parse_mode='Markdown', disable_web_page_preview=True)
-    elif call.data == 'first': choose_group(call)
 
 
 if __name__ == "__main__":
