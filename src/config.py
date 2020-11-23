c04d730706f435f7a224beb7233ce43b3e5e6dc3
Ivan Romancev <dnl.krsvch@gmail.com>
2020-11-23 13:43:23 +0200

    add config and webhook

--- src/config.py
+++ src/config.py
@@ -4,6 +4,11 @@ from os import getenv
 class Config:
     def setUp(self):
         self.token = getenv('TOKEN')
+        self.webhook_host = getenv('WEBHOOK_HOST', '0.0.0.0')
+        self.port = getenv('WEBHOOK_PORT', 8443)
+        self.webhook_listen = getenv('WEBHOOK_LISTEN', '0.0.0.0')
+        self.webhook_url_base = 'https://{}:{}'.format(self.webhook_host, self.port)
+        self.webhook_url_path = '/{}/'.format(self.token)
         self.database_url = getenv('DATABASE_URL')
 
 
